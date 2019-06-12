93400869826d56a46ee39b5aa312d576e1616f35
Tyler Glisson <Tyler@tglisson.com>
2019-06-12 17:38:37 -0400

    Refactor as async function, working version

--- getRepos.js
+++ getRepos.js
@@ -5,41 +5,45 @@ const fetch = require('node-fetch');
 
 // Node file system api
 const fs = require('fs');
-// fs.mkdirSync('./repos');
-
 
 // Node git-clone module
 const clone = require('git-clone');
 
-// Accept github username
-let user = 'TylerGlisson';
-
-const cloneURL_arr = (data) => {
-    const urlArr = data.map((val => val.clone_url));
-    return urlArr;
+const fetcher = async (user) => {
+    return fetch(`https://api.github.com/users/${user}/repos`)
+        .then(data => data.json())
+        .catch(err => {console.log('There was an error ', err)});
 };
 
-const repos = fetch(`https://api.github.com/users/${user}/repos`)
-    .then(response => response.json())
-    // .then(data => console.log(data[0].clone_url))
-    .then((data) => console.log(cloneURL_arr(data)))
-    .catch(err => {console.log('There was an error ', err)});
+const cloneURL_arr = (data) => data.map((val => val.clone_url));
 
-// let rep = repos.then(resJson => console.log(resJson)). 
+const names_arr = (data) => data.map((val => val.name));
 
+let main = async () => {
+    // Accept github username
+    const user = 'TylerGlisson';
 
-// Loop through response array and create a
-//  new array of just repositories
+    // Fetch an object of a user's Github repositories 
+    let response = await fetcher(user);
+    
 
+    // Loop through the response array and create an array of just the urls to clone each repository
+    const urls = cloneURL_arr(response);
 
-// Map through new array and translate to clonable addres by adding ".git"
-// eg     html_url: 'https://github.com/TylerGlisson/Wikipedia-Search',
-//  to -> https://github.com/TylerGlisson/Wikipedia-Search.git
+    // Loop through the response array and create an array of repository names to be used for creating directories
+    const names = names_arr(response);
 
+    // Prompt user for preferred directory to clone repos into
+    const dirName = '/Users/t3/coding/getRepos/repos';
 
-// Prompt user for preferred directory to clone repos into
-let targetPath = './repos';
+    // Loop through array of repository urls, mkdir for each associated reposotory name, then clone repo into new dir
+    urls.map((url, index) => {
+        let path = `${dirName}/${names[index]}`
+       
+        fs.mkdirSync(path);
+        clone(url, path);
+        // console.log(url);
+    });
+};
 
-// Loop through new array and clone each item to preferred directory
-
-// clone(https://github.com/TylerGlisson/Wikipedia-Search.git, targetPath)
+main();

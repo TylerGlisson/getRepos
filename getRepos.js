820fce801903b7f5899dbaf94915e1f31bdb7648
Tyler Glisson <Tyler@tglisson.com>
2019-06-12 11:28:38 -0400

    remove unused scripts

--- getRepos.js
+++ getRepos.js
@@ -3,6 +3,14 @@
 // node-fetch library
 const fetch = require('node-fetch');
 
+// Node file system api
+const fs = require('fs');
+// fs.mkdirSync('./repos');
+
+
+// Node git-clone module
+const clone = require('git-clone');
+
 // Accept github username
 let user = 'TylerGlisson';
 
@@ -14,8 +22,19 @@ const repos = fetch(`https://api.github.com/users/${user}/repos`)
 
 repos.then(resJson => console.log(resJson))
 
+
 // Loop through response array and create a
 //  new array of just repositories
 
 
-// Map through new array and translate  
+// Map through new array and translate to clonable addres by adding ".git"
+// eg     html_url: 'https://github.com/TylerGlisson/Wikipedia-Search',
+//  to -> https://github.com/TylerGlisson/Wikipedia-Search.git
+
+
+// Prompt user for preferred directory to clone repos into
+let targetPath = './repos';
+
+// Loop through new array and clone each item to preferred directory
+
+// clone(https://github.com/TylerGlisson/Wikipedia-Search.git, targetPath)

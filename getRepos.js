1f966e7901f0ef45dfa21bd470823560761fb068
Tyler Glisson <Tyler@tglisson.com>
2019-06-14 15:38:17 -0400

    Remove node_modules foldersd

--- getRepos.js
+++ getRepos.js
@@ -3,7 +3,23 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const clone = require('git-clone');
-const prompt = require('prompt');
+const prompts = require('prompts');
+
+const prompter = async () => {
+    const response = await prompts([
+        {
+        type: 'text',
+        name: 'username',
+        message: 'Please enter a Github username:',
+        },
+        {
+        type: 'text',
+        name: 'dir',
+        message: 'Please enter a local absolute path to clone the repositories into:'
+        }
+    ]);
+    return response;
+};
 
 const fetcher = async (user) => {
     return fetch(`https://api.github.com/users/${user}/repos`)
@@ -15,21 +31,8 @@ const cloneURL_arr = (data) => data.map((val => val.clone_url));
 const names_arr = (data) => data.map((val => val.name));
 
 const main = async () => {
-    // Start the prompt
-    prompt.start();
-
-    // Accept github username
-    const userObj = {
-        userName: '',
-        dirName: ''
-    };
-    prompt.get(['username', 'email'], (err, results) => console.log(results));
-    prompt.addProperties(userObj, ['userName', 'dirName'], err => console.log(userObj));
-
-    const user = 'TylerGlisson';
-
-    // Fetch an object of a user's Github repositories 
-    let response = await fetcher(user);
+    let userObj = await prompter();
+    let response = await fetcher(userObj.username);
     
 
     // Loop through the response array and create an array of just the urls to clone each repository
@@ -38,16 +41,13 @@ const main = async () => {
     // Loop through the response array and create an array of repository names to be used for creating directories
     const names = names_arr(response);
 
-    // Prompt user for preferred directory to clone repos into
-    const dirName = '/Users/t3/coding/repos';
 
     // Loop through array of repository urls, mkdir for each associated reposotory name, then clone repo into new dir
     urls.map((url, index) => {
-        let path = `${dirName}/${names[index]}`
+        let path = `${userObj.dir}/${names[index]}`
        
-        // fs.mkdirSync(path);
-        // clone(url, path);
-        // console.log(url);
+        fs.mkdirSync(path);
+        clone(url, path);
     });
     
 };
